a245c0e72260d8a30e7d1ec1cbfbea21f287b4b8
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-02-17 10:32:33 -0500

    moved App component here and created Routes to Homepage and Upload page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+import "./App.css";
+import Home from "./pages/Home";
+import Upload from "./pages/Upload";
 
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/upload" element={<Upload />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
