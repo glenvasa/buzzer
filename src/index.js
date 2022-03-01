41444ab1836ceb6a8f7cd21b87b5e2782b8e4bfb
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-03-01 09:55:04 -0500

    comments

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./index.css";
+// import "./index.css";
 import "./App.css";
+// import Header from "./components/Header";
 import Home from "./pages/Home";
 import Upload from "./pages/Upload";
 
 const App = () => {
   return (
     <BrowserRouter>
+   {/* wrapping app in Header was causing problems; need to revisit later */}
+      {/* <Header> */}
       <Routes>
         <Route exact path="/" element={<Home />} />
+
         <Route path="/upload" element={<Upload />} />
       </Routes>
+      {/* </Header> */}
     </BrowserRouter>
   );
 };
