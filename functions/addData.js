4e176dd03f1fc3dda4b2de061e9ebc93bb5a209f
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-03-01 09:55:27 -0500

    Create addData.js

---
+++ functions/addData.js
@@ -0,0 +1,9 @@
+const { createClient } = require("@astrajs/rest");
+
+// create an {astra_db} client
+const astraClient = await createClient({
+    astraDatabaseId: process.env.ASTRA_DB_ID,
+    astraDatabaseRegion: process.env.ASTRA_DB_REGION,
+    username: process.env.ASTRA_DB_USERNAME,
+    password: process.env.ASTRA_DB_PASSWORD,
+});
